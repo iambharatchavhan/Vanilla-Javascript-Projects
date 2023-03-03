7765fc3b2618bcec4fb4b57c024c21610ed4f001
Bharat Chavhan <bharatchavhan141@gmail.com>
2023-03-03 16:19:09 +0530

    small project on set interval

--- 1.2Practice/stetTimeInteval/setTimeInterval.js
+++ 1.2Practice/stetTimeInteval/setTimeInterval.js
@@ -2,22 +2,34 @@
 
 const body = document.querySelector("body");
 const btn  = document.querySelector("#btn");
+const btnTwo  = document.querySelector("#btn2");
+const theH1 = document.querySelector(".h1");
 
 
 function randomColors (){
-//    let red = Math.floor(Math.random() * 999) +100;
+   let red = Math.floor(Math.random() * 999) +100;
    let blue= Math.floor(Math.random() * 999) +10;
    let green = Math.floor(Math.random() * 999) +100;
 //    let angle = Math.floor(Math.random() * 360) +30 ;
     // console.log(red ,blue,green);
 
-    colors = `linear-gradient( 90deg ,#${blue},#${green})`
+    colors = `linear-gradient( to left,#${blue},#${red})`
     // console.log(colors);
     body.style.background = colors ;
     //  console.log(but);
 //    console.log(hello);
 }
 
-setInterval(randomColors, 1000)
+const theId = setInterval(randomColors, 1000)
+
+btn.addEventListener('click', function(){
+    console.log("clicked");
+    theH1.textContent = colors;
+   clearInterval (theId);
+})
+
+btnTwo.addEventListener("click",function(){
+    window.location.reload()
+})
 
 
